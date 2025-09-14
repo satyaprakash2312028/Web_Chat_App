c61df94b4712df755b8bfe84ed0209653021c6ed
satyaprakash2312028 <swadeshicreator@gmail.com>
2025-09-14 20:20:05 +0530

    Update axios.js

--- frontEnd/src/lib/axios.js
+++ frontEnd/src/lib/axios.js
@@ -1,6 +1,6 @@
 import axios from "axios"
 const axiosInstance = axios.create({
-    baseURL: "https://web-chat-app-vfne.onrender.com",
+    baseURL: "https://web-chat-app-vfne.onrender.com/api",
     withCredentials: true
 });
 export default axiosInstance

e551cc2bb13d24db1641a13715debf99a2db7ddc
satyaprakash2312028 <swadeshicreator@gmail.com>
2025-09-14 21:30:03 +0530

    Update utils.js

--- backEnd/src/lib/utils.js
+++ backEnd/src/lib/utils.js
@@ -11,7 +11,7 @@ const generateToken = (userId, res) => {
         maxAge: 24*60*60*1000,
         httpOnly: true,
         sameSite: "none",
-        secure: process.env.NODE_ENV !== "development"
+        secure: true
     });
     return token;
 };

7338d6b9196d1dcfcf2a398807383d9d044e4339
satyaprakash2312028 <swadeshicreator@gmail.com>
2025-09-14 21:25:03 +0530

    Update utils.js

--- backEnd/src/lib/utils.js
+++ backEnd/src/lib/utils.js
@@ -10,7 +10,7 @@ const generateToken = (userId, res) => {
     res.cookie("jwt", token, {
         maxAge: 24*60*60*1000,
         httpOnly: true,
-        sameSite: "strict",
+        sameSite: "none",
         secure: process.env.NODE_ENV !== "development"
     });
     return token;
